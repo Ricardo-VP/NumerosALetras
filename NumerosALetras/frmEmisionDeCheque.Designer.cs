4e52d0f5c8478048bed41161887d82bd601b18de
RICARDO VACA <ricardo.vaca@pucese.edu.ec>
2020-12-07 13:02:57 -0500

    Diseño formulario cheque

--- NumerosALetras/frmEmisionDeCheque.Designer.cs
+++ NumerosALetras/frmEmisionDeCheque.Designer.cs
@@ -28,84 +28,111 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
             this.txtOrdenDe = new System.Windows.Forms.TextBox();
             this.txtCantidad = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
             this.txtResultado = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtDestinatario = new System.Windows.Forms.TextBox();
+            this.txtFechaNow = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // txtOrdenDe
+            // 
+            this.txtOrdenDe.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.txtOrdenDe.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtOrdenDe.Font = new System.Drawing.Font("Bookman Old Style", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtOrdenDe.Location = new System.Drawing.Point(159, 212);
+            this.txtOrdenDe.MaxLength = 50;
+            this.txtOrdenDe.Name = "txtOrdenDe";
+            this.txtOrdenDe.Size = new System.Drawing.Size(481, 32);
+            this.txtOrdenDe.TabIndex = 1;
+            this.txtOrdenDe.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtOrdenDe_KeyPress);
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.txtCantidad.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtCantidad.Font = new System.Drawing.Font("Bookman Old Style", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCantidad.Location = new System.Drawing.Point(725, 212);
+            this.txtCantidad.MaxLength = 9;
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(198, 39);
+            this.txtCantidad.TabIndex = 2;
+            this.txtCantidad.TextChanged += new System.EventHandler(this.txtCantidad_TextChanged);
+            this.txtCantidad.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtCantidad_KeyPress);
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.txtResultado.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtResultado.Font = new System.Drawing.Font("Arial Narrow", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResultado.Location = new System.Drawing.Point(37, 272);
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.ReadOnly = true;
+            this.txtResultado.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtResultado.Size = new System.Drawing.Size(857, 31);
+            this.txtResultado.TabIndex = 4;
+            this.txtResultado.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(386, 485);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(195, 38);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "HACER CHEQUE";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Arial Narrow", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(27, 76);
+            this.label1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.label1.Enabled = false;
+            this.label1.Font = new System.Drawing.Font("Lucida Sans Unicode", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(666, 348);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(215, 29);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Páguese a la orden de:";
+            this.label1.Size = new System.Drawing.Size(219, 34);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Por: Ricardo VP";
             // 
-            // txtOrdenDe
+            // txtDestinatario
             // 
-            this.txtOrdenDe.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtOrdenDe.Location = new System.Drawing.Point(248, 75);
-            this.txtOrdenDe.Name = "txtOrdenDe";
-            this.txtOrdenDe.Size = new System.Drawing.Size(318, 30);
-            this.txtOrdenDe.TabIndex = 1;
+            this.txtDestinatario.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.txtDestinatario.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtDestinatario.Font = new System.Drawing.Font("Bookman Old Style", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDestinatario.Location = new System.Drawing.Point(87, 350);
+            this.txtDestinatario.MaxLength = 50;
+            this.txtDestinatario.Name = "txtDestinatario";
+            this.txtDestinatario.Size = new System.Drawing.Size(330, 32);
+            this.txtDestinatario.TabIndex = 7;
             // 
-            // txtCantidad
+            // txtFechaNow
             // 
-            this.txtCantidad.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtCantidad.Location = new System.Drawing.Point(248, 133);
-            this.txtCantidad.MaxLength = 9;
-            this.txtCantidad.Name = "txtCantidad";
-            this.txtCantidad.Size = new System.Drawing.Size(176, 30);
-            this.txtCantidad.TabIndex = 2;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Arial Narrow", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(27, 133);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(146, 29);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "La cantidad de:";
-            // 
-            // txtResultado
-            // 
-            this.txtResultado.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtResultado.Location = new System.Drawing.Point(76, 195);
-            this.txtResultado.Multiline = true;
-            this.txtResultado.Name = "txtResultado";
-            this.txtResultado.ReadOnly = true;
-            this.txtResultado.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.txtResultado.Size = new System.Drawing.Size(542, 133);
-            this.txtResultado.TabIndex = 4;
-            // 
-            // button1
-            // 
-            this.button1.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(461, 129);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(92, 38);
-            this.button1.TabIndex = 5;
-            this.button1.Text = "Calcular";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.txtFechaNow.AutoSize = true;
+            this.txtFechaNow.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.txtFechaNow.Font = new System.Drawing.Font("Lucida Sans Unicode", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFechaNow.Location = new System.Drawing.Point(681, 137);
+            this.txtFechaNow.Name = "txtFechaNow";
+            this.txtFechaNow.Size = new System.Drawing.Size(0, 23);
+            this.txtFechaNow.TabIndex = 8;
             // 
             // frmEmisionDeCheque
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(703, 353);
+            this.BackgroundImage = global::NumerosALetras.Properties.Resources.chequedesign21;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(973, 535);
+            this.Controls.Add(this.txtFechaNow);
+            this.Controls.Add(this.txtDestinatario);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.txtResultado);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.txtCantidad);
             this.Controls.Add(this.txtOrdenDe);
-            this.Controls.Add(this.label1);
             this.Name = "frmEmisionDeCheque";
             this.Text = "Emisión de Cheque";
             this.ResumeLayout(false);
@@ -114,12 +141,12 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox txtOrdenDe;
         private System.Windows.Forms.TextBox txtCantidad;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox txtResultado;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtDestinatario;
+        private System.Windows.Forms.Label txtFechaNow;
     }
 }
--- NumerosALetras/frmEmisionDeCheque.cs
+++ NumerosALetras/frmEmisionDeCheque.cs
@@ -20,8 +20,62 @@ namespace NumerosALetras
         private void button1_Click(object sender, EventArgs e)
         {
             //UTILS.Clases.NumerosLetras.TestUnidades();
+            if (txtCantidad.Text.Length == 0)
+            {
+                MessageBox.Show("Ingresa el valor del cheque", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                txtCantidad.Focus();
+                return;
+            }
+            else if (txtOrdenDe.Text.Length == 0)
+            {
+                MessageBox.Show("Ingresa el nombre del remitente", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                txtOrdenDe.Focus();
+                return;
+            }
+            else if (txtDestinatario.Text.Length == 0)
+            {
+                MessageBox.Show("Ingresa el nombre del destinatario", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                txtDestinatario.Focus();
+                return;
+            }
+            else { 
             int num = Int32.Parse(this.txtCantidad.Text);
-            this.txtResultado.Text = UTILS.Clases.NumerosLetras.getMillones(num);
+            this.txtResultado.Text = UTILS.Clases.NumerosLetras.getMillones(num) + " dólares";
+            txtFechaNow.Text = Convert.ToString(DateTime.Now.ToString("dd 'de' MMMM 'del' yyyy"));
+            }
+
+        }
+
+        private void txtCantidad_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtOrdenDe_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!(char.IsLetter(e.KeyChar)) && (e.KeyChar != (char)Keys.Back))
+            {
+                MessageBox.Show("Solo se permiten letras", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                e.Handled = true;
+                return;
+            }
+        }
+
+        private void txtCantidad_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (Char.IsDigit(e.KeyChar))
+            {
+                e.Handled = false;
+            }
+            else if (Char.IsControl(e.KeyChar))
+            {
+                e.Handled = false;
+            }
+            else
+            {
+                e.Handled = true;
+                MessageBox.Show("Solo números enteros", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
         }
     }
 }
