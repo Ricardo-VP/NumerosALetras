81369e27479be205c4e5446fc167c45d3029cd22
RICARDO VACA <ricardo.vaca@pucese.edu.ec>
2020-12-07 09:26:19 -0500

    Agregar archivos de proyecto.

---
+++ NumerosALetras/Clases/NumerosLetras.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UTILS.Clases
+{
+    class NumerosLetras
+    {
+        String[] Unidades = {"cero", "uno", "dos", "tres", "cuatro", "cinco",
+                             "seis", "siete","ocho","nueve","diez","once","doce",
+                             "trece","catorce","quince","dieciseis","diecisiete",
+                             "deiciocho","diecinueve"};
+
+        String[] Decenas = {"diez", "veinte", "treinta", "cuarenta", "cincuenta",
+                             "sesenta","setenta","ochenta","noventa"};
+
+        String[] Centenas = {"cien","doscientos","trescientos","cuatrocientos","quinientos",
+                             "seiscientos","setecientos","ochocientos","novecientos"};
+    }
+}
+    
---
+++ NumerosALetras/frmEmisionDeCheque.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace NumerosALetras
+{
+    partial class frmEmisionDeCheque
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtOrdenDe = new System.Windows.Forms.TextBox();
+            this.txtCantidad = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(27, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(215, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Páguese a la orden de:";
+            // 
+            // txtOrdenDe
+            // 
+            this.txtOrdenDe.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtOrdenDe.Location = new System.Drawing.Point(248, 75);
+            this.txtOrdenDe.Name = "txtOrdenDe";
+            this.txtOrdenDe.Size = new System.Drawing.Size(318, 30);
+            this.txtOrdenDe.TabIndex = 1;
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCantidad.Location = new System.Drawing.Point(248, 133);
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(318, 30);
+            this.txtCantidad.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(27, 133);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(146, 29);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "La cantidad de:";
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResultado.Location = new System.Drawing.Point(76, 195);
+            this.txtResultado.Multiline = true;
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.ReadOnly = true;
+            this.txtResultado.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtResultado.Size = new System.Drawing.Size(542, 133);
+            this.txtResultado.TabIndex = 4;
+            // 
+            // frmEmisionDeCheque
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(703, 353);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtCantidad);
+            this.Controls.Add(this.txtOrdenDe);
+            this.Controls.Add(this.label1);
+            this.Name = "frmEmisionDeCheque";
+            this.Text = "Emisión de Cheque";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtOrdenDe;
+        private System.Windows.Forms.TextBox txtCantidad;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtResultado;
+    }
+}
